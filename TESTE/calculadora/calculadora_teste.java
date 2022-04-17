08537529b6957f49c9760fdf2d4a6b0a80cd2184
Natan Bueno <natanbueno@id.uff.br>
2022-04-17 20:49:43 -0300

    Adição de display names

--- TESTE/calculadora/calculadora_teste.java
+++ TESTE/calculadora/calculadora_teste.java
@@ -2,12 +2,14 @@ package calculadora;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Testes do pacote calculadora")
 public class calculadora_teste {
 	
-	//teste
 	
+	@DisplayName("Testa a soma de dois números")
 	@Test
 	public void testSomaDoisNumeros() {
 		Calculadora calc = new Calculadora();
@@ -15,6 +17,7 @@ public class calculadora_teste {
 		Assertions.assertEquals(9, soma);		
 	}
 	
+	@DisplayName("Testa a divisão de dois números")
 	@Test
 	public void testDivisaoDoisNumeros() {
 		Calculadora calc = new Calculadora();
